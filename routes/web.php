afdfa936c3d5709d7207c39e5b59221b76e36fea
Pengfei Peng <pengfeipeng@PengfeimatoMacBook-Pro.local>
2018-06-26 16:44:19 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/','StaticPagesController@home');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
+
+Route::get('/abc', function () {
     return view('welcome');
 });
