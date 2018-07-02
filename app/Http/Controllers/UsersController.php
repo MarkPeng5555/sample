7ae30532a38e68df9f3d66df778656b948993f0a
Pengfei Peng <pengfeipeng@PengfeimatoMacBook-Pro.local>
2018-07-02 10:08:03 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,42 @@
  */
 
 namespace App\Http\Controllers;
+use App\Models\User;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
-
-class UsersController
+class UsersController extends Controller
 {
 
     public function create()
     {
-        return view('layouts/create');
+        return view('users.create');
     }
+
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> bcrypt($request->password),
+            ]
+
+        );
+        Session()->flash('success','注册成功');
+        return redirect()->route('users.show',[$user]);
+    }
+
+
 }
