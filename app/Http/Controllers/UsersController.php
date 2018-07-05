f4aa109b6864b123d21bcaba72b718d6ed8b7408
Pengfei Peng <pengfeipeng@PengfeimatoMacBook-Pro.local>
2018-07-05 14:46:37 +0800

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,9 +11,19 @@ use App\Models\User;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\AuthenticationException;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store','index']
+        ]);
+    }
 
     public function create()
     {
@@ -40,8 +50,59 @@ class UsersController extends Controller
             ]
 
         );
+        Auth::login($user);
         Session()->flash('success','注册成功');
         return redirect()->route('users.show',[$user]);
+
+
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function edit(User $user)
+    {
+        try {
+
+
+            $this->authorize('update', $user);
+        }
+        catch (AuthorizationException $authorizationException) {
+            return view('users.edit', compact('user'));
+        }
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+//        $users= User::all();
+        return view('users.index',compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 
 
